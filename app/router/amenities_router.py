8613d4ca020165a1510fbc1ee16fda5f1bf29414
Daniel Quaidoo <daniel.quaidoo@gmail.com>
2024-05-14 13:37:55 +0000

    Add fix for linking properties to ammenities and media

--- app/router/amenities_router.py
+++ app/router/amenities_router.py
@@ -3,9 +3,10 @@ from uuid import UUID
 from fastapi import Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.models import Amenities
+from app.models import Amenities, EntityMedia, EntityAmenities
 from app.dao.ammenities_dao import AmenitiesDAO
 from app.schema import AmmenitiesSchema
+from app.utils import DAOResponse
 from app.router.base_router import BaseCRUDRouter
 
 class AmmenitiesRouter(BaseCRUDRouter):
@@ -18,16 +19,18 @@ class AmmenitiesRouter(BaseCRUDRouter):
     def register_routes(self):
         @self.router.post("/link_property_to_media")
         async def add_property_media(property_unit_assoc_id: UUID, media_id: UUID, db: AsyncSession = Depends(self.get_db)):
-            property_media = await self.dao.link_property_to_media(db_session=db, property_unit_assoc_id=property_unit_assoc_id, media_id=media_id)
+            property_media : EntityMedia = await self.dao._link_property_to_media(db_session=db, property_unit_assoc_id=property_unit_assoc_id, media_id=media_id)
 
             if property_media is None:
                 raise HTTPException(status_code=404, detail="Error adding media to property")
-            return property_media
+            
+            return DAOResponse(success=True, data=property_media.to_dict())
         
         @self.router.post("/link_property_to_ammenity")
         async def add_property_ammenity(property_unit_assoc_id: UUID, ammenity_id: UUID, db: AsyncSession = Depends(self.get_db)):
-            property_ammenity = await self.dao.link_property_to_ammenity(db_session=db, property_unit_assoc_id=property_unit_assoc_id, ammenity_id=ammenity_id)
-            # 1e06dfe8-b25a-4998-bc3f-1eb5e5eff9dd
+            property_ammenity : EntityAmenities = await self.dao._link_property_to_ammenity(db_session=db, property_unit_assoc_id=property_unit_assoc_id, ammenity_id=ammenity_id)
+            
             if property_ammenity is None:
                 raise HTTPException(status_code=404, detail="Error adding ammenity to property")
-            return property_ammenity
+            
+            return DAOResponse(success=True, data=property_ammenity.to_dict())
