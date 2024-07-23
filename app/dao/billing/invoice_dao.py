c796a9e81e048a3b3f29052388fe81036639cc6b
Daniel Quaidoo <daniel.quaidoo@gmail.com>
2024-07-23 23:19:39 +0000

    Update invoices

--- app/dao/billing/invoice_dao.py
+++ app/dao/billing/invoice_dao.py
@@ -71,10 +71,9 @@ class InvoiceDAO(BaseDAO[Invoice]):
             # commit object to db session
             await self.commit_and_refresh(db_session, new_invoice)
 
-            # return DAOResponse[InvoiceResponse](
-            #     success=True, data=InvoiceResponse.from_orm_model(new_invoice)
-            # )
-            return DAOResponse[InvoiceResponse](success=True, data=[])
+            return DAOResponse[InvoiceResponse](
+                success=True, data=InvoiceResponse.from_orm_model(new_invoice)
+            )
         except ValidationError as e:
             return DAOResponse(success=False, data=str(e))
         except Exception as e:
